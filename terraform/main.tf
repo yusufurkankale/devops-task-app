1ab8f8db7b74df657cac2bf2b8754951ebb8c1ef
Yusuf Furkan Kale <80515246+yusufurkankale@users.noreply.github.com>
2026-04-03 15:17:41 +0300

    Frontend API URL ve Terraform Security Group guncellendi

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,13 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
